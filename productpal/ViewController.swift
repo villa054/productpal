7286ff00bb25ec649b2e98218e7afa6cfc550ad9
villa054 <ivan_is_live@live.com>
2016-05-09 10:39:07 -0500

    Camera functionality

--- productpal/ViewController.swift
+++ productpal/ViewController.swift
@@ -9,7 +9,60 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func onNewPhoto(sender: AnyObject) {
+   
+        let actionSheet = UIAlertController (title: "New Photo", message:nil, preferredStyle: .ActionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+            self.showCamera()
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: { action in
+            self.showAlbum()
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Canccel", style: .Cancel, handler: nil
+            
+            ))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    func showCamera(){
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .Camera
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    
+    func showAlbum(){
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .PhotoLibrary
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as?UIImage{
+            imageView.image = image
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
